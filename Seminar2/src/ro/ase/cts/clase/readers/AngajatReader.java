3597aa5bce1a85f19648467f975f5ba68b811ab6
RebecaNedelcu <nedelcu.rebeca@gmail.com>
2021-03-04 17:24:11 +0200

    final seminar 2

--- Seminar2/src/ro/ase/cts/clase/readers/AngajatReader.java
+++ Seminar2/src/ro/ase/cts/clase/readers/AngajatReader.java
@@ -7,12 +7,19 @@ import java.util.List;
 import java.util.Scanner;
 
 import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
 
-public class AngajatReader {
-	public static List<Angajat> readAngajati(String file) throws FileNotFoundException {
-		Scanner input2 = new Scanner(new File(file));
+public class AngajatReader extends AplicantReader  {
+	
+	public AngajatReader(String fileName) {
+		super(fileName);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Aplicant> readAplicanti() throws FileNotFoundException {
+		Scanner input2 = new Scanner(new File(super.fileName));
 		input2.useDelimiter(",");
-		List<Angajat> angajati = new ArrayList<Angajat>();
+		List<Aplicant> angajati = new ArrayList<Aplicant>();
 
 		while (input2.hasNext()) {
 			String nume = input2.next();
@@ -31,5 +38,6 @@ public class AngajatReader {
 		input2.close();
 		return angajati;
 	}
+	
 
 }
--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -4,15 +4,22 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.clase.readers.AngajatReader;
+import ro.ase.cts.clase.readers.AplicantReader;
+import ro.ase.cts.program.Program;
 
 public class Program {
+	
+	public static List<Aplicant> citesteAplicanti(AplicantReader reader ) throws FileNotFoundException{
+		return reader.readAplicanti();
+	}
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = AngajatReader.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = citesteAplicanti(new AngajatReader("angajati.txt"));
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
