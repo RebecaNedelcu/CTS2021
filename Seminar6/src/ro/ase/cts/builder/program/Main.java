e96596dce09b4402497c1990ca6d54b576b9d6ac
RebecaNedelcu <nedelcu.rebeca@gmail.com>
2021-05-14 01:11:10 +0300

    builder + prototype

---
+++ Seminar6/src/ro/ase/cts/builder/program/Main.java
@@ -0,0 +1,34 @@
+package ro.ase.cts.builder.program;
+
+import ro.ase.cts.builder.clase.Rezervare;
+import ro.ase.cts.builder.clase.RezervareBuilder;
+import ro.ase.cts.builder.clase.RezervareBuilderV2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rezervare rezervare1 = new RezervareBuilder().setCodRezervare(20).setAreBauturaInclusa(true).build();
+		Rezervare rezervare2 = new RezervareBuilder().setCodRezervare(30).setAreMuzicaAmbientala(true)
+				.setGenMuzica("populara").build();
+		Rezervare rezervare3 = new Rezervare(true, true, false, false, null, 50);
+
+		RezervareBuilder newBuilder = new RezervareBuilder().setAreMancareInclusa(true).setAreBauturaInclusa(true)
+				.setAreScaunErgonomic(true);
+		Rezervare rezervare4 = newBuilder.setCodRezervare(100).build();
+		Rezervare rezervare5 = newBuilder.setCodRezervare(101).build();
+
+		System.out.println(rezervare1);
+		System.out.println(rezervare2);
+		System.out.println(rezervare3);
+		System.out.println(rezervare4);
+		System.out.println(rezervare5);
+
+		RezervareBuilderV2 builderV2 = new RezervareBuilderV2().setAreMancareInclusa(true).setAreBauturaInclusa(true)
+				.setAreScaunErgonomic(true);
+		Rezervare rezervare6 = builderV2.setCodRezervare(102).build();
+		Rezervare rezervare7 = builderV2.setCodRezervare(103).build();
+
+		System.out.println(rezervare6);
+		System.out.println(rezervare7);
+	}
+}
