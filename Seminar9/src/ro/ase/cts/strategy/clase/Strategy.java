26bba141e80d0410a045652a6eba069927511fd0
RebecaNedelcu <nedelcu.rebeca@gmail.com>
2021-05-14 01:14:21 +0300

    flyweight + strategy

---
+++ Seminar9/src/ro/ase/cts/strategy/clase/Strategy.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.strategy.clase;
+
+public interface Strategy {
+	public void plateste(float suma);
+}
---
+++ Seminar9/src/ro/ase/cts/strategy/program/Main.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.strategy.program;
+
+import ro.ase.cts.strategy.clase.Card;
+import ro.ase.cts.strategy.clase.Client;
+import ro.ase.cts.strategy.clase.Portofel;
+import ro.ase.cts.strategy.clase.Strategy;
+
+public class Main {
+	public static void main(String[] args) {
+		Client client1 = new Client("rbk");
+		Client client2 = new Client("nedelcu");
+		Strategy strategieCash = new Portofel(500);
+		Strategy strategieCard = new Card();
+		client1.setStrategie(strategieCash);
+		client2.setStrategie(strategieCard);
+		
+		client1.platesteNota(300);
+		client2.platesteNota(180);
+		
+		client1.platesteNota(400);
+		client1.setStrategie(strategieCard);
+		client1.platesteNota(400);
+	}
+}
